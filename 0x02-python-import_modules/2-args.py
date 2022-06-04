3b06457a58c3c15389e979ed2df203fd055d4178
Hiruy3 <hiruytesfaye321@gmail.com>
2022-06-04 10:07:17 -0700

    python task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-	if len(argv) == 2:
-		print("1 argument:")
-		print("1: {:s}".format(argv[1]))
-	else:
-		print("{:d} arguments:".format(len(argv)-1))
-		for i in range(len(argv[1:])):
-			print("{:d}: {:s}".format(i+1,argv[i+1]))
+    """Print the number of and list of arguments."""
+    import sys
 
-
-
-
-
-
-
-
-
-"""
-import sys
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("{:d} arguments".format(len(sys.argv) - 1))
-    elif len(sys.argv) == 2:
-        print("{:d} argument:".format(len(sys.argv) - 1))
-        print("{:d}: {:s}".format(len(sys.argv) - 1, sys.argv[1]))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
-        for i in range(len(sys.argv) - 1):
-            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
-"""
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
