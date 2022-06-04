9f579276eb17c5f817b2c6f176713c8c6ff28f51
Hiruy3 <hiruytesfaye321@gmail.com>
2022-06-04 11:13:26 -0700

    python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from sys import argv
-	li = list(map(int,argv[1:]))
-	print("{:d}".format(sum(li)))
-
-
-
-
-"""from sys import argv
-if __name__ == "__main__":
-    x = 0
-    for i in range(len(argv) - 1):
-        x += int(argv[i + 1])
-    print("{:d}".format(x))
-"""
+    import sys
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
